9ff7cd3da40e2965357d3514c18a09a91cb108a4
Manavk2004 <mjkamdar04@gmail.com>
2026-01-10 15:03:31 -0500

    Fixed type error

--- trpc/server.tsx
+++ trpc/server.tsx
@@ -1,5 +1,5 @@
 import 'server-only' // <-- ensure this file cannot be imported from the client
-import { createTRPCOptionsProxy, TRPCQueryOptions } from '@trpc/tanstack-react-query';
+import { createTRPCOptionsProxy, TRPCQueryOptions, type ResolverDef } from '@trpc/tanstack-react-query';
 import { cache } from 'react';
 import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
@@ -23,7 +23,7 @@ export function HydrateClient(props: { children: React.ReactNode }) {
   );
 }
 
-export function prefetch<T extends ReturnType<TRPCQueryOptions<unknown>>>(
+export function prefetch<T extends ReturnType<TRPCQueryOptions<ResolverDef>>>(
   queryOptions: T,
 ) {
   const queryClient = getQueryClient();
